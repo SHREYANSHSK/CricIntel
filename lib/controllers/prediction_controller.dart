24b931902e69baf65781dc3b7e27ec0b708a2bdf
khandelwal.happy.hk <khandelwal.happy.hk@gmail.com>
2025-04-12 03:54:44 +0000

    changes in controller

--- lib/controllers/prediction_controller.dart
+++ lib/controllers/prediction_controller.dart
@@ -1,7 +1,27 @@
 import 'package:get/get.dart';
 
 class PredictionController extends GetxController {
-  void updatePrediction() {
-    // Placeholder for updating prediction logic
+  // Observable variables for match data
+  RxString teams = "".obs;
+  RxString tournament = "".obs;
+  RxString venue = "".obs;
+  RxString winProbability = "".obs;
+  RxString favoredTeam = "".obs;
+
+  // Method to update match data
+  void updateMatchData(Map<String, String> data) {
+    if (data.containsKey("teams")) {
+      teams.value = data["teams"]!;
+    }
+    if (data.containsKey("tournament")) {
+      tournament.value = data["tournament"]!;
+    }
+    if (data.containsKey("venue")) {
+      venue.value = data["venue"]!;
+    }
+    if (data.containsKey("winProbability")) {
+      winProbability.value = data["winProbability"]!;
+      favoredTeam.value = data["favoredTeam"]!;
+    }
   }
 }
